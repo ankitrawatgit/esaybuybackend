0100f15b594dd583a51cc94d18e1b07098308b38
Ankit <anrwt9389@gmail.com>
2024-03-04 22:42:43 -0500

    createuser. todo return jwt after create user

--- src/Routes/Auths/signup.ts
+++ src/Routes/Auths/signup.ts
@@ -2,34 +2,25 @@ import { Router, Request, Response } from "express";
 import UserService from "../../Services/UserServices";
 import { body, validationResult } from "express-validator";
 
-const signuprouter =  Router();
+const signuprouter = Router();
 
-signuprouter.post('/createuser',[
-    body('name').isString().isLength({min:4})
+signuprouter.post('/createuser', [
+    body('name').isString().isLength({ min: 4 }),
+    body('email').isEmail(),
+    body('password').isString().isLength({ min: 6 }),
 ]
-,(req:Request,res:Response)=>{   
-    //Validation Error check
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({error:"Validation Error occured!"});
-}
-    
-    //Accessing cookies
-    console.log("Name Cookies:",req.cookies.yournameis)
-    console.log("Limited Cookies:",req.cookies.limitedtimecookie)
+    , async (req: Request, res: Response) => {
+        //Validation Error check
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: "Validation Error occured!" });
+        }
+        const { name, email, password, image } = req.body;
 
-    //create User
-    UserService.createUser({
-        name:req.body.name
-    });
-    
-    res.cookie('yournameis', req.body.name);
-    //setting cookies
-    res.cookie('limitedtimecookie', 'BORA', {
-        maxAge: 3600000, // Cookie will expire after 1 hour
-        // httpOnly: true, // Cookie is accessible only by the server, not by client-side scripts
-      });
-    res.send('done');
-})
+        //create User
+        await UserService.createUser({
+            name, email, password, image
+        }, res);
+    })
 
 export default signuprouter;
--- src/Services/UserServices.ts
+++ src/Services/UserServices.ts
@@ -1,10 +1,62 @@
+import { Response } from "express";
+import prismaClient from "../db/prismaclient";
+import bcrypt from 'bcrypt';
+
+const saltRounds = 100;
+
+
 interface userDataIntereface{
-    name:String
+    name:string,
+    email:string,
+    password:string,
+    image: string | null
 }
 
 class UserService{
-    public static async createUser(params:userDataIntereface){
-        console.log("Hello ",params.name);
+    
+    private static async generateHash(password:string):Promise<string>{
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(password, 10, (err: any, hash: any) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(hash);
+                }
+            });
+        });
+    
+    }
+    
+    
+    public static async getUserByEmail(email:string){
+        try {
+            return await prismaClient.user.findFirst({where:{email:email}})
+        } catch (error) {
+            throw new Error("Something went wrong");
+        }
+    }
+
+    public static async createUser(params:userDataIntereface,res:Response){
+     try {
+        
+        // const user = await this.getUserByEmail(params.email);
+        // if(user){
+        //     throw new Error("User alrady Exists");
+        // }
+
+        const hashedpass = await this.generateHash(params.password);
+       
+        const user = await prismaClient.user.create({data:{name:params.name,email:params.email,password:hashedpass,image:params.image}})
+        
+        console.log(user);
+
+        return res.status(200).json({message:'User created!'})
+        
+     } catch (error:any) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal Server Error", errorMessage: error.message });
+     }
+        
     }
 }
 
